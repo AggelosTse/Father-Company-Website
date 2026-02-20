288c20df6a396eb999e7ebfc57533963d336fed2
EiriniGero <irenegerodaki@gmail.com>
2026-02-20 19:11:11 +0200

    top

--- website/src/components/texts.jsx
+++ website/src/components/texts.jsx
@@ -1,4 +1,4 @@
-import '../styling/top.css';
+import '../styling/texts.css';
 import '../styling/middle.css';
 
 export function Name(){
@@ -8,15 +8,13 @@ export function Name(){
 export function CenterText(){
     return(
       <div className="center-wrapper">
-        <h2 className='centerText'>ΣΤΟΧΟΣ ΜΑΣ Η ΨΥΧΑΓΩΓΙΑ ΣΑΣ</h2>
-        <h1 className='centerSubText'>Με πολλά χρόνια εμπειρίας στον χώρο.</h1>
+        <b className='centerText'>ΣΤΟΧΟΣ ΜΑΣ Η ΨΥΧΑΓΩΓΙΑ ΣΑΣ</b>
+        <b className='centerSubText'>Με πολλά χρόνια εμπειρίας στον χώρο.</b>
       </div>
     );
   }
   
 export function ScrollText(){
-    return <h2 className='scrollText'>ΕΛΑΤΕ ΝΑ ΣΥΝΕΡΓΑΣΤΟΥΜΕ:</h2>;
+    return <b className='scrollText'>ΕΛΑΤΕ ΝΑ ΣΥΝΕΡΓΑΣΤΟΥΜΕ:</b>;
   }
 
-
-  
