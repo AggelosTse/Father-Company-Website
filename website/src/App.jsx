6af717ebf31c10d1f74d89d6eb7f9b1090ac5db2
AggelosTse <aggu8914@gmail.com>
2026-02-20 20:49:22 +0200

    Aggelos - adding button, centralizing elements

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,13 +1,17 @@
 import {Name, CenterText} from './components/texts.jsx';
 import ProjectCarousel from './components/carousel.jsx';
-
+import { EmailButton } from './components/emailform.jsx';
 
 function App() {
   return (
-    <div className="main-layout">
+    <div>
       <Name/> 
+
       <CenterText/> 
       <ProjectCarousel/>
+
+      <EmailButton/>
+
     </div>
   );
 }
---
+++ website/src/components/emailform.jsx
@@ -0,0 +1,10 @@
+import './styling/bottom.css';
+
+export function EmailButton(){
+    return(
+        <div className="emailbutton">
+            <button>Click Me!</button> 
+        </div>
+        
+    );
+}
