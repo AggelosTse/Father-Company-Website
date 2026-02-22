cb85cb3256bb6c057c53034b3f8f81b95377abca
AggelosTse <aggu8914@gmail.com>
2026-02-22 16:09:37 +0200

    Aggelos - email form made better

--- website/src/components/emailPage/emailform.jsx
+++ website/src/components/emailPage/emailform.jsx
@@ -1,6 +1,8 @@
+import '../../styling/middle.css';
+
 export default function EmailForm(){
     return(
-        <div>
+        <form className="emailFields">
         <label htmlFor="email">Email </label>
         <input type="email" id="email" name="email" required />
 
@@ -8,7 +10,7 @@ export default function EmailForm(){
                 <textarea id="mess" name="mess" rows="4"></textarea>
 
                 <input type="submit" value="Submit" />
-      </div>
+      </form>
       
     );
 }
