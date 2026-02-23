fd09707a4cfe1ee04d0c9aaa1b5dbd51bd04a8a1
AggelosTse <aggu8914@gmail.com>
2026-02-23 12:37:48 +0200

    Aggelos - post request made

--- website/src/components/emailPage/emailform.jsx
+++ website/src/components/emailPage/emailform.jsx
@@ -1,16 +1,57 @@
-import '../../styling/middle.css';
+import { useState } from "react";
+import "../../styling/middle.css";
 
-export default function EmailForm(){
-    return(
-        <form className="emailFields">
-        <label htmlFor="email">Email </label>
-        <input type="email" id="email" name="email" required />
+export default function EmailForm() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-        <label htmlFor="mess">Message: </label>
-                <textarea id="mess" name="mess" rows="4"></textarea>
+  async function sendDataToServer(e) {
+    e.preventDefault();
+    const userInfo = {
+      userEMail: email,
+      userMessage: message,
+    };
 
-                <input type="submit" value="Submit" />
-      </form>
-      
-    );
+    const response = await fetch("/sendEmail", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userInfo),
+    });
+
+    if(!response.ok){
+        console.error("failed");
+        return;
+      }
+
+      const serverMessage = await response.json();
+
+  }
+
+ 
+
+
+
+  return (
+    <form className="emailFields" onSubmit={sendDataToServer}>
+      <label htmlFor="email">Email </label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <label htmlFor="mess">Message: </label>
+      <textarea
+        id="mess"
+        name="mess"
+        rows="4"
+        onChange={(e) => setMessage(e.target.value)}
+      ></textarea>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
